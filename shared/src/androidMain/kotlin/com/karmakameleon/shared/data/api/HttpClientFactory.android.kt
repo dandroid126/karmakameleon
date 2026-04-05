961ac9541d2397d3250a3c9c0009cb81d1ed698a
dandroid <mail@dandroid.app>
2026-04-04 20:03:09 -0700

    Added cookie handling. r/all was broken without it.

--- shared/src/androidMain/kotlin/com/karmakameleon/shared/data/api/HttpClientFactory.android.kt
+++ shared/src/androidMain/kotlin/com/karmakameleon/shared/data/api/HttpClientFactory.android.kt
@@ -2,6 +2,9 @@ package com.karmakameleon.shared.data.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
 
 actual fun createHttpClient(): HttpClient {
     return HttpClient(OkHttp) {
@@ -9,7 +12,20 @@ actual fun createHttpClient(): HttpClient {
             config {
                 followRedirects(true)
                 retryOnConnectionFailure(true)
+                cookieJar(InMemoryCookieJar())
             }
         }
+    }
+}
+
+private class InMemoryCookieJar : CookieJar {
+    private val cookieStore = mutableMapOf<String, MutableList<Cookie>>()
+
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        cookieStore[url.host] = cookies.toMutableList()
+    }
+
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        return cookieStore[url.host] ?: emptyList()
     }
 }
